d253968171c753363f771faf952322ae51c3e26e
PigeonInPanic <ruslan.grafchikov@mail.ru>
2023-01-03 01:54:25 +0400

    C# start
    Ladno

---
+++ FCA/FisrtConsoleApps/Program.cs
@@ -0,0 +1,18 @@
+﻿var people = new string[] { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+
+// находим индекс элемента "Bob"
+int bobIndex = Array.BinarySearch(people, "Bob");
+// находим индекс первого элемента "Tom"
+int tomFirstIndex = Array.IndexOf(people, "Tom");
+// находим индекс последнего элемента "Tom"
+int tomLastIndex = Array.LastIndexOf(people, "Tom");
+// находим индекс первого элемента, у которого длина строки больше 3
+int lengthFirstIndex = Array.FindIndex(people, person => person.Length > 3);
+// находим индекс последнего элемента, у которого длина строки больше 3
+int lengthLastIndex = Array.FindLastIndex(people, person => person.Length > 3);
+
+Console.WriteLine($"bobIndex: {bobIndex}");                 // 2
+Console.WriteLine($"tomFirstIndex: {tomFirstIndex}");       // 0
+Console.WriteLine($"tomLastIndex: {tomLastIndex}");         // 4
+Console.WriteLine($"lengthFirstIndex: {lengthFirstIndex}"); // 3
+Console.WriteLine($"lengthLastIndex: {lengthLastIndex}");   // 5
