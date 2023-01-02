6e41adddb3be24ec84af3b3a7ef727a751b51e08
PigeonInPanic <ruslan.grafchikov@mail.ru>
2023-01-03 01:36:35 +0400

    Create first file python
    Lalala

---
+++ I remember github/array.py
@@ -0,0 +1,9 @@
+example_array = [[[-1, 0], [0, 1]], [[2, 3], [5, 8]]]
+print(example_array[0])
+print(example_array[1])
+print(example_array[0][1])
+print(example_array[0][1][0])
+# Вывод:
+[[-1, 0], [0, 1]]
+[[2, 3], [5, 8]]
+[0, 1]
